e37a4754f56b0a9dac8a4bcc1a156fb80913e5b9
eric yuyitung <eyuyitung@gmail.com>
2017-07-11 14:56:23 -0400

    all first run information retrieved and stored as a list of active instances

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from googleapiclient import discovery
 import google.auth
 import os
+import pprint
 
 project_root = os.getcwd()
 os.environ ["GOOGLE_APPLICATION_CREDENTIALS"] = project_root + "\\google-credentials.json"
@@ -12,11 +13,33 @@ rm = discovery.build('cloudresourcemanager', 'v1',credentials=credentials)
 comp = discovery.build('compute', 'v1', credentials=credentials)
 
 def main() :
-    project = rm.projects().list().execute()
-    print project['projects'][0]['projectId']
-    zone = comp.zones().list({project['projects'][0]['projectId'][0]}).execute()
-#  print zone
+    instances = []
+    
+    proreq = rm.projects().list()
+    prores = proreq.execute()
+    proarr = prores['projects'][0]
+    project_name = proarr['name']
+    project_id = proarr['projectId']
+    zonereq = comp.zones().list(project = project_id)
+    zoneres = zonereq.execute()
 
-
+    for zone in zoneres['items'] :
+        zone_name = zone['name']
+        instreq = comp.instances().list(project = project_id, zone = zone_name)
+        instres = instreq.execute()
+        if 'items' in instres:
+            instance_name =(instres['items'][0]['name'])
+            status =(instres['items'][0]['status'])
+            machineurl = instres['items'][0]['machineType']
+            machineType = ""
+            for key in machineurl :
+                if key == '/' :
+                    machineType = ""
+                else :
+                    machineType += key
+            instances.append ({'project name' : project_name, 'zone name' : zone_name,
+                                'active instances' : instance_name,'instance status' : status,
+                                'machine type' : machineType})
+    print instances
 if __name__ == '__main__':
     main()
