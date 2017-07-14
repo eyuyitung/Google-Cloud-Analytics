9f2ce9635bf01537d24e2e5e4b84016f62871889
eric yuyitung <eyuyitung@gmail.com>
2017-07-13 22:30:54 -0400

    New iteration of rewrite added monitorin and optimization
    optimization of for loops and modular system for collecting monitoring
    data added debug statements into main(). should now run properly within
    pycharm without termin , if not,  edit working directory.

--- src/main.py
+++ src/main.py
@@ -1,57 +1,76 @@
-from googleapiclient import discovery
 from google.cloud import monitoring
+from googleapiclient import discovery
 import google.auth
 import os
-import pprint
+import json
+from pprint import pprint
 
-project_root = os.getcwd()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = project_root + "\\google-credentials.json"
+
+project_root = os.path.abspath(os.path.join(__file__ ,"../.."))
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = project_root + os.path.sep + "google-credentials.json"
 
 credentials, project = google.auth.default()
 
-rm = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
-comp = discovery.build('compute', 'v1', credentials=credentials)
+resource_manager = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
+compute = discovery.build('compute', 'v1', credentials=credentials)
 
-def get_monitoring_client(project) :
-    return monitoring.Client(project = project,credentials=credentials)
+instance_metrics = {'cpu utilization': 'instance/cpu/utilization',  # concatenates with compute api base url
+                        'disk read bytes': 'instance/disk/read_bytes_count',
+                        'disk read operations': 'instance/disk/read_ops_count',
+                        'disk write bytes': 'instance/disk/write_bytes_count',
+                        'disk write operations': 'instance/disk/write_ops_count',
+                        'received bytes': 'instance/network/received_bytes_count',
+                        'received packets': 'instance/network/received_packets_count',
+                        'sent bytes': 'instance/network/sent_bytes_count',
+                        'sent packets': 'instance/network/sent_packets_count'}
 
 
+def get_monitoring_client(project):
+    return monitoring.Client(project=project, credentials=credentials)
 
 
 def main():
-    proreq = rm.projects().list()
-    prores = proreq.execute()
-    proarr = prores['projects'][0]
-    project_name = proarr['name']
-    project_id = proarr['projectId']
-    zonereq = comp.zones().list(project=project_id)
-    zoneres = zonereq.execute()
-    instances = []
-    for zone in zoneres['items']:
-        zone_name = zone['name']
-        instreq = comp.instances().list(project=project_id, zone=zone_name)
-        instres = instreq.execute()
-        if 'items' in instres:
-            client = get_monitoring_client(project_id,zone = zone_name)
-            METRIC = 'compute.googleapis.com/instance/cpu/utilization'
-            query =(client.query(METRIC,minutes= 5))
-            print(query.as_dataframe())
-            instance_name = (instres['items'][0]['name'])
-            status = (instres['items'][0]['status'])
-            machineurl = instres['items'][0]['machineType']
-            machineType = ""
-            for key in machineurl:
-                if key == '/':
-                    machineType = ""
-                else:
-                    machineType += key
-            instances.append({'project name': project_name,
-                              'zone name': zone_name,
-                              'active instances': instance_name,
-                              'instance status': status,
-                               'machine type': machineType})
+    data = {} # TODO dump project list back into data dict
+    data['projects'] = api_call(resource_manager.projects(), 'projects', [])
+    print "Found %d projects" % len(data['projects'])
+    for project in data['projects']:  # for each project in the list of project dictionaries :
+        project['instances'] = []  # add another key : value pair into project dictionary
+        project_id = project['projectId']
 
-        else: instances.append(zone_name)
-    pprint.pprint(instances)
+        all_zones = api_call(compute.zones(), 'items', {'project': project_id})
+        print "Found %d zones" % len(all_zones)
+        for zone in all_zones:  # for each zone in list of zone dictionaries :
+            zone_name = zone['name']
+            current_instances = api_call(compute.instances(), 'items', {'project': project_id, 'zone': zone_name})
+            if current_instances is not None and len(current_instances) > 0:
+                project['instances'].extend(current_instances)
+        print "Found %d instances" % len(project['instances'])
+        project['metrics'] = {}
+        for key in instance_metrics:  # TODO Associate metrics with respective instance
+            project['metrics'][key] = monitoring_call(project_id, key)
+
+
+def api_call(base, key, args): # generic method for pulling relevant data from api response
+    export = []
+    if args:
+        request = base.list(**args)
+    else:
+        request = base.list()
+
+    while request is not None:
+        response = request.execute()
+        if key in response:
+            export.extend(response[key])
+        request = base.list_next(previous_request=request, previous_response=response)
+    return export
+
+
+def monitoring_call(project_id, metric):
+    client = get_monitoring_client(project_id)
+    METRIC = 'compute.googleapis.com/' + instance_metrics[metric]
+    query = client.query(METRIC, minutes=5)
+    return query.as_dataframe()
+
+
 if __name__ == '__main__':
     main()
