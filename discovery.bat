1103b24a97bc7a4e48afe5587ec7e9210aafb24c
eric yuyitung <eyuyitung@gmail.com>
2017-08-15 17:01:50 -0400

    more rework of discovery.bat

--- discovery.bat
+++ discovery.bat
@@ -12,13 +12,19 @@ rem **************** edit nothing beyond this point *********************
 
 echo Copyright (c) 2002-2017 Cirba Inc. D/B/A Densify. All Rights Reserved.
 set AUTO="f"
+set FILE="f"
+set MANUAL="f"
 IF "%1"=="--auto" set AUTO="t"
 IF "%1"=="-a" set AUTO="t"
-set FILE="f"
 IF "%1"=="--file" set FILE="t"
 IF "%1"=="-f" set FILE="t"
+IF %FILE%=="t" (
+	set PROJECT_ID=%2
+	set MANUAL="t"
+)
 
 IF "%1"=="" (
+	set MANUAL="t"
 	set /p PROJECT_ID=Please enter full credential file name: [ex. my-project-123.json] 
 
 	echo one week is 168 hours, month is 720 hours, cannot sample beyond 6 weeks [1008 hours]
@@ -26,49 +32,53 @@ IF "%1"=="" (
 
 	echo Within the project specified above, do any of the instances contain active stackdriver agents?
 	set /p AGENTS=Please enter [Y/N] : 
+)
 
+IF %MANUAL%=="t" (
+	echo __________________Manually Loading %PROJECT_ID%_____________________________
+	call:discoveryFunc %PROJECT_ID% %HOURS% %AGENTS%
+	GOTO:EOF
 )
 
 set fpath=%~sdp0
-
-ECHO  - Step 1 - GCP Discovery
 IF %AUTO%=="t" (
-	for /f "delims=|" %%f in ('dir /b %fpath%credentials') DO (
-		:LOOP
+	for /f %%f in ('dir /b %fpath%credentials') do (
 		set PROJECT_ID=%%f 
-		GOTO PROGRAM
+		echo __________________Automatically Loading %PROJECT_ID%_____________________________
+		call:discoveryFunc %PROJECT_ID% %HOURS% %AGENTS% 
 	)
 )
+GOTO:EOF
 
-:PROGRAM
+:discoveryFunc
+echo %~1
+ECHO  - Step 1 - GCP Discovery
+py %fpath%src\main.py -i %~1 -t %~2 -a %~3 
 
-py %fpath%src\main.py -a %AGENTS% -t %HOURS% -i %PROJECT_ID%
-
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
 
 ECHO  - Step 2 - GCP Config
 move %fpath%gcp_config.csv %fpath%conf\gcp_config.csv
 move %fpath%attributes.csv %fpath%conf\attributes.csv
 move %fpath%workload.csv %fpath%conf\workload.csv
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
 
 ECHO  - Step 3 - Creating Manifest
 call "%CIRBA_HOME%\bin\audit-converter.bat" -m %fpath%conf\ "GCP"
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
 
 ECHO  - Step 4 - Creating Repository
 call "%CIRBA_HOME%\bin\audit-converter.bat" %fpath%conf\ %fpath%repo\
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
 
 ECHO  - Step 5 - Loading Repository
 call "%CIRBA_HOME%\bin\load-repository.bat" %fpath%repo\ -o
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
 
 ECHO  - Step 6 - Import Attributes
 call "%CIRBA_HOME%\bin\data-tools.bat" ImportAttributes -f %fpath%conf\
-if errorlevel 1 (GOTO END)
+if errorlevel 1 GOTO:EOF
+GOTO:EOF
 
-
-:END
+:EOF
 pause
-
