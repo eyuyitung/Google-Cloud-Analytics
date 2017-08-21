389263b054f75ccf8142c83b0a3eb9dcd788469e
jackhillier <jhillier@densify.com>
2017-08-21 18:12:26 -0400

    EOF is built in, so END will let us go the where you define as END

--- discovery.bat
+++ discovery.bat
@@ -55,37 +55,41 @@ if %HELP%=="t" (
 	echo no arguments : specifiy all of the parameters manually 
 )
 
-GOTO:EOF
+GOTO:END
 
 :discoveryFunc
+del %fpath%gcp_config.csv
+del %fpath%attributes.csv
+del %fpath%workload.csv
+
 ECHO  - Step 1 - GCP Discovery
 python %fpath%src\main.py -i %~1 -t %~2 -a %~3 
 
-if errorlevel 1 GOTO:EOF
+if errorlevel 1 GOTO:END
 
 ECHO  - Step 2 - GCP Config
 move %fpath%gcp_config.csv %fpath%conf\gcp_config.csv
 move %fpath%attributes.csv %fpath%conf\attributes.csv
 move %fpath%workload.csv %fpath%conf\workload.csv
-if errorlevel 1 GOTO:EOF
+if errorlevel 1 GOTO:END
 
 ECHO  - Step 3 - Creating Manifest
 call "%CIRBA_HOME%\bin\audit-converter.bat" -m %fpath%conf\ "GCP"
-if errorlevel 1 GOTO:EOF
+if errorlevel 1 GOTO:END
 
 ECHO  - Step 4 - Creating Repository
 call "%CIRBA_HOME%\bin\audit-converter.bat" %fpath%conf\ %fpath%repo\
-if errorlevel 1 GOTO:EOF
+if errorlevel 1 GOTO:END
 
 ECHO  - Step 5 - Loading Repository
 call "%CIRBA_HOME%\bin\load-repository.bat" %fpath%repo\ -o
-if errorlevel 1 GOTO:EOF
+if errorlevel 1 GOTO:END
 
 ECHO  - Step 6 - Import Attributes
 call "%CIRBA_HOME%\bin\data-tools.bat" ImportAttributes -f %fpath%conf\
-if errorlevel 1 GOTO:EOF
-GOTO:EOF
+if errorlevel 1 GOTO:END
+GOTO:END
 
-:EOF
+:END
 if errorlevel 1 echo Exiting program...
 pause
